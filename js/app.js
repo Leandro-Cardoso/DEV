64194b8a189d151fc62110d3bee7cb4f3e3bd4ff
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-09-22 01:10:07 -0300

    Load components

---
+++ js/app.js
@@ -0,0 +1,25 @@
+async function loadComponent(elementId, componentUrl) {
+
+    try {
+
+        const response = await fetch(componentUrl);
+        const html = await response.text();
+
+        document.getElementById(elementId).innerHTML = html;
+
+    } catch (error) {
+
+        console.error('Erro ao carregar', elementId, error);
+
+    }
+}
+
+// Load components:
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //loadComponent('header-container', 'components/header.html');
+    loadComponent('div-about', 'components/div-about.html');
+    //loadComponent('footer-container', 'components/footer.html');
+
+});
