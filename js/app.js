05462bf38c87c9f85aa2ffe01454fb52cfa6bbbf
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-10-10 23:05:11 -0300

    Menu mobile mode

--- js/app.js
+++ js/app.js
@@ -1,24 +1,54 @@
-async function loadComponent(elementId, componentUrl) {
-
+// Load components:
+async function loadComponent(elementId, componentUrl, callback = () => {}) {
     try {
-
         const response = await fetch(componentUrl);
         const html = await response.text();
-
         document.getElementById(elementId).innerHTML = html;
-
+        callback(); 
     } catch (error) {
-
         console.error('Erro ao carregar', elementId, error);
-
     }
 }
 
-// Load components:
+// Menu:
+function toggleMenu(id) {
 
+    const menu = document.getElementById(id);
+    
+    menu.classList.toggle('hide');
+
+}
+
+function initializeMenuLogic() {
+    const menuImg = document.getElementById('menu-img');
+    const menu = document.getElementById('menu');
+    const mediaQuery = window.matchMedia('(max-width: 600px)');
+    const classHide = 'hide';
+    const classMobileMenu = 'mobile-menu';
+
+    function handleScreenResize(e) {
+        if (e.matches) {
+            // Mobile mode:
+            menu.classList.add(classHide);
+            menu.classList.add(classMobileMenu);
+            menuImg.classList.remove(classHide);
+        } else {
+            // PC mode:
+            menu.classList.remove(classHide);
+            menu.classList.remove(classMobileMenu);
+            menuImg.classList.add(classHide);
+        }
+    }
+
+    handleScreenResize(mediaQuery);
+    mediaQuery.addListener(handleScreenResize);
+}
+
+// Events:
 document.addEventListener('DOMContentLoaded', () => {
 
-    loadComponent('header', './components/header.html');
+    // Load components:
+    loadComponent('header', './components/header.html', initializeMenuLogic);
     loadComponent('div-about', './components/div-about.html');
     loadComponent('div-techs', './components/div-techs.html');
     loadComponent('div-academic-qualifications', './components/div-academic-qualifications.html');
@@ -26,5 +56,4 @@ document.addEventListener('DOMContentLoaded', () => {
     loadComponent('div-certifications', './components/div-certifications.html');
     loadComponent('div-work-experience', './components/div-work-experience.html');
     loadComponent('footer', './components/footer.html');
-
 });
